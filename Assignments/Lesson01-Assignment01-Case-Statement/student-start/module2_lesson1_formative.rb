3414d29791e4c14b71487c70c872dec8db11b0d1
WanderDataRuby <wrmfrm007@yahoo.com>
2015-09-30 15:49:47 -0400

    Update module2_lesson1_formative.rb

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,3 +1,8 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
 some_var = "false"
 another_var = "nil"
 
@@ -13,3 +18,28 @@ elsif some_var == false
 else
   puts "I guess nothing matched... But why?"
 end
+
+some_var = "false"
+another_var = "nil"
+
+case 
+when some_var == "pink elephant"
+  puts "Don't think about the pink elephant!"
+
+when another_var.nil?
+  puts "Question mark in the method name?"
+
+when some_var == false
+  puts "Looks like this one should execute"
+
+else
+  puts "I guess nothing matched... But why?"
+end
+
+    
+#because in the first condition, it is checked if the variable is equal to the string "pink elephant",
+# because the content is not boolean, never run, checking the three conditions equally, 
+# the second must be a string called nill not a constant, 
+# and 3 is not true because it is a constant call false is not a 
+# string therefore none is true and the last condition is executed, 
+# since all others are not true, they should compare a string, not constant.
