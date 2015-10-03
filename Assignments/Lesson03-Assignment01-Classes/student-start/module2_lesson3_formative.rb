fe1bd2b7544aa7a24fd6c3b6517036b32fe2828e
WanderDataRuby <wrmfrm007@yahoo.com>
2015-10-03 12:57:58 -0400

    Update module2_lesson3_formative.rb
    I have used other methods to practice with these, and make it more interactive with the user, using what he asks of course, I hope not bother to use other attributes.

--- Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
+++ Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
@@ -34,3 +34,78 @@ puts Person.search("Smith")
 # Should print out
 # => John Smith
 # => Jane Smith
+#coding: utf-8
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Person  
+attr_accessor :firsname,:lastname, :year, :sex
+def initialize(fnamep,lastnamep,yearp,sexp)
+    self.firsname = fnamep
+    self.lastname=lastnamep
+    self.year = yearp
+    self.sex = sexp
+    @@personsarray << self
+  end
+  def self.search(last_name)
+    @@personsarray.select {|person| person.lastname == last_name}
+  end  
+ @@personsarray = []
+end
+answer=0
+while answer.to_i!=2
+  
+
+puts "¿What Your Name? " 
+$stdout.flush 
+yourname = gets.chomp
+
+puts "¿What Your LastName? " 
+$stdout.flush 
+yourlastname = gets.chomp
+
+puts "¿What you Age? "
+$stdout.flush 
+yourage = gets.chomp.to_i
+
+puts "¿What your Sex? "
+$stdout.flush 
+yoursex = gets.chomp
+
+  puts "\nyour name is: #{yourname},your lastname #{yourlastname} your age #{yourage.to_s} your sex #{yoursex}\n"
+puts "you are not yet the object\n"
+puts "now i made the object, of the class person, and i'll identify with the letter a \n"
+a = Person.new(yourname, yourlastname, yourage, yoursex)
+puts "rigth now  "+" --- a --- "+" it's a class ------> #{a.class}\n"
+puts "the metho's are:\n"
+a.methods
+puts "now you can see the atribute  ---> name, lastname, year and sex\n"
+puts "the name is : ---- #{a.firsname} ---"
+puts "for assing the name that has introduction before is: a.nombre = llamas"
+a.firsname = yourname
+puts a.firsname
+puts "now the name is: ---- #{a.firsname} ---\n"
+puts "the lastname is: ---- #{a.lastname} ---\n"
+puts "now the year is: ---- #{a.year} ---\n"
+puts "now the sex is: ---- #{a.sex} ---\n"
+puts "for assigne year and sex \n"
+puts "you lastname gived a.lastname = yourlastname\n"
+a.year = yourlastname
+puts "you age gived a.year = yourage\n"
+a.year = yourage
+puts "and introduced your sex a.sex= yoursex\n"
+a.sex = yoursex
+puts "now the name is : ---- #{a.firsname} ---, your lastname #{a.lastname}, your age --- #{a.year}--- and your sex #{a.sex}\n" 
+b=Person.new("Wander","Rondon",23,"mas")
+c=Person.new("Manuel","Rone",24,"mas")
+d=Person.new("Jessi","smith",45,"Fem")
+e=Person.new("Michel","garcia",34,"fem")
+puts 'Looking for:'
+namelooking=gets.chomp()
+puts Person.search(namelooking).to_s
+puts 'Other in (1-yes, 2-No)'
+answer=gets.chomp().to_i
+puts `clear`
+end
